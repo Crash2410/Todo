09d7f65805e6932d47591fb600cc66997b51064e
Artem <artem.xolod.90@gmail.com>
2021-07-14 13:28:56 +0300

    проверка входящих данных

--- src/components/post-list/post-list.jsx
+++ src/components/post-list/post-list.jsx
@@ -5,15 +5,28 @@ import './post-list.css';
 const PostList = ({ data }) => {
 
     const elements = data.map((post) => {
-        const { id, ...postProps } = post;
-        return (
-            <li key={id} className="list-group-item">
-                <PostListItem
-                    {...postProps}
-                />
-            </li>
-        );
-    })
+        if (typeof post === 'object' && !isEmptyObj(post)) {
+            const { id, ...postProps } = post;
+            return (
+                <li key={id} className="list-group-item">
+                    <PostListItem
+                        {...postProps}
+                    />
+                </li>
+            );
+        } else {
+            return null;
+        }
+    });
+    // Проверка на пустой объект
+    function isEmptyObj(object) {
+        for (let key in object) {
+            if (object.hasOwnProperty(key)) {
+                return false;
+            }
+        }
+        return true;
+    }
 
     return (
         <ul className="app-list list-group">
